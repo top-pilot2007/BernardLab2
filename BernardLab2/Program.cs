adc9eb9e105872942098950782caf3504cf139fd
Bernard Polynice <bepoly.nice@gmail.com>
2020-07-13 17:27:08 -0400

    Fix While Loop, made it shorter

--- BernardLab2/Program.cs
+++ BernardLab2/Program.cs
@@ -8,53 +8,29 @@ namespace BernardLab2
         {
             Console.WriteLine("Welcome to Grand Circus' Room Detail Generator!");
             
-            Console.WriteLine("Enter Length: ");
-            var userLength = Console.ReadLine();
-            double length = Convert.ToDouble(userLength);
+            var continueYesNo = "y";
 
-            Console.WriteLine("Enter Width: ");
-           var userWidth = Console.ReadLine();
-           double width = Convert.ToDouble(userWidth);
-
-           double area = length * width;
-            double perimeter = length + length + width + width;
-
-           Console.WriteLine($"Area {area}");
-           Console.WriteLine($"Perimeter {perimeter}");
-
-           Console.WriteLine("Continue? (y/n) ");
-           var continueYesNo = Console.ReadLine();
-
-             
-            while
-                (continueYesNo == "y") 
-            
+            while (continueYesNo == "y")
             {
-                Console.WriteLine("Enter Length: ");
-                var ContinueUserLength = Console.ReadLine();
-                double ContinueLength = Convert.ToDouble(ContinueUserLength);
+                Console.Write("Enter Length: ");
+                var userLength = Console.ReadLine();
+                double length = Convert.ToDouble(userLength);
 
-                Console.WriteLine("Enter Width: ");
-                var ContinueUserWidth = Console.ReadLine();
-                double ContinueWidth = Convert.ToDouble(ContinueUserWidth);
+                Console.Write("Enter Width: ");
+                var userWidth = Console.ReadLine();
+                double width = Convert.ToDouble(userWidth);
 
-                double ContinueArea = ContinueLength * ContinueWidth;
-                double ContinuePerimeter = ContinueLength + ContinueLength + ContinueWidth + ContinueWidth;
+                double area = length * width;
+                double perimeter = length + length + width + width;
 
-                Console.WriteLine($"Area {ContinueArea}");
-                Console.WriteLine($"Perimeter {ContinuePerimeter}");
+                Console.WriteLine($"Area {area}");
+                Console.WriteLine($"Perimeter {perimeter}");
 
-               Console.WriteLine("Continue? (y/n) ");
-                var stillContinueYesNo = Console.ReadLine();
-                if
-                    (stillContinueYesNo == "n") { Console.WriteLine("Goodbye");
-                    break;
-                }
-
+                
+                Console.WriteLine("Continue? (y/n) ");
+                continueYesNo = Console.ReadLine().ToLower().Trim();
             }
-            
-            if 
-                    (continueYesNo== "n") { Console.WriteLine("Goodbye"); }
+            Console.WriteLine("Goodbye!");
         }
     }
 }
